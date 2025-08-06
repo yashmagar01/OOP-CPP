40094ee79833e5934a5e11397b643bc2df015e7e
yash magar <yashajaymagar10@gmail.com>
2025-08-06 22:04:44 +0530

    Added offical lab manual

---
+++ yash_m/Practicals/09/table/02.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class Employee
+{
+    int id;
+    char name[30];
+
+public:
+    void getdata();
+
+    void putdata();
+};
+
+void Employee::getdata()
+{
+    cout << "Enter Id : ";
+    cin >> id;
+    cout << "Enter Name : ";
+    cin >> name;
+}
+void Employee::putdata()
+{
+    cout << id << " ";
+    cout << name << " ";
+    cout << endl;
+}
+int main()
+{
+    Employee emp[30];
+    int n, i;
+    cout << "Enter Number of Employees - ";
+    cin >> n;
+
+    for (i = 0; i < n; i++)
+        emp[i].getdata();
+
+    cout << "Employee Data - " << endl;
+
+    for (i = 0; i < n; i++)
+        emp[i].putdata();
+}
