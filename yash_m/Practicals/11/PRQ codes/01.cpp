b38a8ae99d6058add6ed962dc15fcd58463a7a49
yash magar <yashajaymagar10@gmail.com>
2025-08-21 23:50:05 +0530

    Completed PRQ 1 of 11

--- yash_m/Practicals/11/PRQ codes/01.cpp
+++ yash_m/Practicals/11/PRQ codes/01.cpp
@@ -12,10 +12,47 @@ class student
         int roll_no;
         string name;
 
-        
+        void read()
+        {
+            cout << "\nEnter Roll no.: ";
+            cin >> roll_no;
+            cout << "ENter your name: ";
+            cin >> name;
+        }
+};
+
+class Marks: public student
+{
+    public:
+        float m1, m2, m3, total, per;;
+
+        void read()
+        {
+            student::read();
+            cout << "\nEnter marks of Three subjects: ";
+            cin >> m1 >> m2 >> m3;
+        }
+        void cal()
+        {
+            total = (m1 + m2 + m3);
+            per = total / 3.0;
+        }
+        void display()
+        {
+            cout << "\nRoll no.: " << roll_no << endl;
+            cout << "Name: " << name << endl;
+            cout << "Total Marks: " << total << endl;
+            cout << "Percentage: " << per << "%" << endl;
+        }
 };
 
 int main()
 {
+    Marks s1;
+
+    s1.read();
+    s1.cal();
+    s1.display();
+
     return 0;
 }
